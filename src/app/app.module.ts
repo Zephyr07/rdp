7159775f3b1b1f76ed714c92dcf6a2da3687e041
Edward NANDA <edward.nanda@wiicod.com>
2019-04-04 23:02:58 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,46 @@
-import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
-
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
-import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-
-import { StatusBar } from '@ionic-native/status-bar';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { RamassagePage } from '../pages/ramassage/ramassage';
+import { ConfirmationRamassagePage } from '../pages/confirmation-ramassage/confirmation-ramassage';
+import { InfosPage } from '../pages/infos/infos';
+import { MentionsLegalesPage } from '../pages/mentions-legales/mentions-legales';
+import { ProposPage } from '../pages/propos/propos';
+import { SuggestionPage } from '../pages/suggestion/suggestion';
+import { ServicesPage } from '../pages/services/services';
+import { ChoixDomicilePage } from '../pages/choix-domicile/choix-domicile';
+import { ComptePage } from '../pages/compte/compte';
+import { InformationsPersonnellePage } from '../pages/informations-personnelle/informations-personnelle';
+import { AbonnementsPage } from '../pages/abonnements/abonnements';
+import { ReabonnementPage } from '../pages/reabonnement/reabonnement';
+import { DomicilesPage } from '../pages/domiciles/domiciles';
+import { AjoutDomicilePage } from '../pages/ajout-domicile/ajout-domicile';
+import { EditInfosPage } from '../pages/edit-infos/edit-infos';
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    RamassagePage,
+    ConfirmationRamassagePage,
+    InfosPage,
+    MentionsLegalesPage,
+    ProposPage,
+    SuggestionPage,
+    ServicesPage,
+    ChoixDomicilePage,
+    ComptePage,
+    InformationsPersonnellePage,
+    AbonnementsPage,
+    ReabonnementPage,
+    DomicilesPage,
+    AjoutDomicilePage,
+    EditInfosPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +49,22 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    RamassagePage,
+    ConfirmationRamassagePage,
+    InfosPage,
+    MentionsLegalesPage,
+    ProposPage,
+    SuggestionPage,
+    ServicesPage,
+    ChoixDomicilePage,
+    ComptePage,
+    InformationsPersonnellePage,
+    AbonnementsPage,
+    ReabonnementPage,
+    DomicilesPage,
+    AjoutDomicilePage,
+    EditInfosPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { RamassagePage } from '../ramassage/ramassage';
+import { InfosPage } from '../infos/infos';
+import { ServicesPage } from '../services/services';
+import { ComptePage } from '../compte/compte';
 
 @Component({
   selector: 'page-home',
@@ -9,6 +13,22 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  onGoToRamasage(){
+  	this.navCtrl.push(RamassagePage);
+  }
+
+  onGoToInfos(){
+    this.navCtrl.push(InfosPage);
+  }
+
+  onGoToService(){
+    this.navCtrl.push(ServicesPage);
+  }
+
+  onGoToCompte(){
+    this.navCtrl.push(ComptePage);
   }
 
 }
